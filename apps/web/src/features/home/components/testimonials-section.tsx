0abe450dc11d6d967a69e8018f4c90513504fd73
Doug Silkstone <doug@withseismic.com>
2024-12-06 13:11:33 +0100

    feat(web): fix testimonial responsive design

--- apps/web/src/features/home/components/testimonials-section.tsx
+++ apps/web/src/features/home/components/testimonials-section.tsx
@@ -64,7 +64,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-    <div className="relative isolate pb-16 pt-16 sm:pb-24 sm:pt-24 lg:pb-32 lg:pt-32">
+    <div className="relative isolate pb-32 pt-24 sm:pt-32">
       <div
         aria-hidden="true"
         className="absolute inset-x-0 top-1/2 -z-10 -translate-y-1/2 transform-gpu overflow-hidden opacity-30 blur-3xl"
@@ -79,14 +79,14 @@ export const TestimonialsSection = () => {
       </div>
       <div
         aria-hidden="true"
-        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden opacity-25 blur-3xl"
+        className="absolute inset-x-0 top-0 -z-10 flex transform-gpu overflow-hidden pt-32 opacity-25 blur-3xl sm:pt-40 xl:justify-end"
       >
         <div
           style={{
             clipPath:
               'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
           }}
-          className="relative left-[calc(50%-11rem)] aspect-[1313/771] w-[82.0625rem] rotate-[30deg] bg-gradient-to-tr from-primary to-primary/60 sm:left-[calc(50%-30rem)] lg:left-[calc(50%-38rem)]"
+          className="ml-[-22rem] aspect-[1313/771] w-[82.0625rem] flex-none origin-top-right rotate-[30deg] bg-gradient-to-tr from-primary to-primary/60 xl:ml-0 xl:mr-[calc(50%-12rem)]"
         />
       </div>
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -94,16 +94,16 @@ export const TestimonialsSection = () => {
           <h2 className="text-base/7 font-semibold text-primary">
             Real Results
           </h2>
-          <p className="mt-2 text-3xl font-semibold tracking-tight sm:text-4xl lg:text-5xl">
+          <p className="mt-2 text-balance text-3xl font-semibold tracking-tight sm:text-4xl lg:text-5xl">
             See how teams are improving their rankings with Onsite
           </p>
         </div>
-        <div className="mx-auto mt-8 grid max-w-2xl gap-4 sm:mt-12 lg:mt-16 lg:max-w-none">
-          <figure className="rounded-2xl bg-muted p-6 shadow-lg ring-1 ring-muted lg:p-8">
+        <div className="mx-auto mt-8 flex flex-col gap-4 text-sm/6 sm:mt-12 lg:mt-16 lg:max-w-none lg:grid lg:grid-flow-col lg:grid-cols-4 lg:grid-rows-1">
+          <figure className="rounded-2xl bg-muted p-6 shadow-lg ring-1 ring-muted sm:p-8 lg:col-span-2 lg:col-start-2 lg:row-end-1">
             <blockquote className="text-base font-semibold tracking-tight sm:text-lg lg:text-xl">
               <p>{`"${featuredTestimonial.body}"`}</p>
             </blockquote>
-            <figcaption className="mt-6 flex items-center gap-4 border-t border-muted-foreground/10 pt-6">
+            <figcaption className="mt-6 flex flex-wrap items-center gap-x-4 gap-y-4 border-t border-muted-foreground/10 pt-6">
               <img
                 alt=""
                 src={featuredTestimonial.author.imageUrl}
@@ -124,12 +124,24 @@ export const TestimonialsSection = () => {
               )}
             </figcaption>
           </figure>
-
-          <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
-            {testimonials.map((columnGroup, columnGroupIdx) => (
-              <div key={columnGroupIdx} className="space-y-4">
-                {columnGroup.map((column) =>
-                  column.map((testimonial) => (
+          {testimonials.map((columnGroup, columnGroupIdx) => (
+            <div
+              key={columnGroupIdx}
+              className="flex flex-col gap-4 lg:contents"
+            >
+              {columnGroup.map((column, columnIdx) => (
+                <div
+                  key={columnIdx}
+                  className={cn(
+                    'flex flex-col gap-4',
+                    (columnGroupIdx === 0 && columnIdx === 0) ||
+                      (columnGroupIdx === testimonials.length - 1 &&
+                        columnIdx === columnGroup.length - 1)
+                      ? 'lg:row-span-2'
+                      : 'lg:row-start-1',
+                  )}
+                >
+                  {column.map((testimonial) => (
                     <figure
                       key={testimonial.author.handle}
                       className="rounded-2xl bg-muted p-6 shadow-lg ring-1 ring-muted"
@@ -137,7 +149,7 @@ export const TestimonialsSection = () => {
                       <blockquote>
                         <p className="text-sm sm:text-base">{`"${testimonial.body}"`}</p>
                       </blockquote>
-                      <figcaption className="mt-6 flex items-center gap-4">
+                      <figcaption className="mt-6 flex items-center gap-x-4">
                         <img
                           alt=""
                           src={testimonial.author.imageUrl}
@@ -153,11 +165,11 @@ export const TestimonialsSection = () => {
                         </div>
                       </figcaption>
                     </figure>
-                  )),
-                )}
-              </div>
-            ))}
-          </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
